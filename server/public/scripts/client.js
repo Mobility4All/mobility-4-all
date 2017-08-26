d1e12b17b6085ef609375e8d2c39d2d15a3d7c12
Tom Brossart <tom@blueraven.digital>
2017-08-26 15:46:13 -0500

    troubleshooting google maps api integration

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMap']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMap', 'ngAutocomplete']);
 
 /// Routes ///
 myApp.config(function($routeProvider, $locationProvider, $mdThemingProvider) {
--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -1,10 +1,20 @@
-myApp.controller('HomeController', function($location) {
+myApp.controller('HomeController', function($location, $scope, NgMap) {
     console.log('HomeController created');
     var hc = this;
 
     hc.go = function(url) {
       console.log(url);
       $location.path(`/${url}`)
-    }
+    };
+
+    $scope.result1 = 'initial value';
+    $scope.options1 = null;
+    $scope.details1 = '';
+
+    NgMap.getMap().then(function(map) {
+      console.log(map.getCenter());
+      console.log('markers', map.markers);
+      console.log('shapes', map.shapes);
+  });
 
 });
