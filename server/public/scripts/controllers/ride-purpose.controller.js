0ac1721d3570c5d8a2d30da48c22e578824edfec
Peter Prentiss <peter.prentiss@gmail.com>
2017-08-28 16:30:15 -0500

    Basic rider purpose page functional.

--- server/public/scripts/controllers/ride-purpose.controller.js
+++ server/public/scripts/controllers/ride-purpose.controller.js
@@ -1,5 +1,22 @@
-myApp.controller('RidePurposeController', function() {
+myApp.controller('RidePurposeController', function(DataService, $location) {
     console.log('RidePurposeController created');
     var rc = this;
+    rc.ride = {};
+    rc.message = '';
 
+    rc.updatePurpose = function(purpose) {
+      rc.ride.purpose = purpose;
+      console.log('ride purpose', rc.ride.purpose);
+    }
+
+    rc.confirmPurpose = function() {
+      console.log('confirming purpose');
+      if (rc.ride.purpose) {
+        DataService.rideObject.purpose = rc.ride.purpose;
+        console.log('data ride:', DataService.rideObject.purpose);
+        $location.path('/input-ride')
+      } else {
+        rc.message = 'Please select a purpose'
+      }
+    }
 });
