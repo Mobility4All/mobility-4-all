798d4deadd6b15a917dceec1afa08b9570559aec
Tom Brossart <tom@blueraven.digital>
2017-08-28 17:11:55 -0500

    geocoordinates logging out

--- server/public/scripts/controllers/ride-purpose.controller.js
+++ server/public/scripts/controllers/ride-purpose.controller.js
@@ -2,6 +2,8 @@ myApp.controller('RidePurposeController', function() {
     console.log('RidePurposeController created');
     var rpc = this;
 
+    geolocate();
+
     var placeSearch, autocomplete;
     var componentForm = {
       street_number: 'short_name',
@@ -30,28 +32,51 @@ myApp.controller('RidePurposeController', function() {
     // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
     function fillInAddress() {
-      // Get the place details from the autocomplete object.
+      var geocoder = new google.maps.Geocoder();
       var place = autocomplete.getPlace();
 
-      for (var component in componentForm) {
-        document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false;
-      }
+      var lat = place.geometry.location.lat(),
+          lng = place.geometry.location.lng();
 
-      // Get each component of the address from the place details
-      // and fill the corresponding field on the form.
-      for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        if (componentForm[addressType]) {
-          var val = place.address_components[i][componentForm[addressType]];
-          document.getElementById(addressType).value = val;
-        }
-      }
+      console.log("coordinates are:", place, lat, lng);
+      // geocoder.geocode({'address': place})
+
+
+
+
+
+
+
+
+
+
+
+// EXAMPLES FILL IN
+      // // Get the place details from the autocomplete object.
+      // var place = autocomplete.getPlace();
+      // console.log('place is:', place);
+      // var coordinates = place.geolocation;
+      // console.log("coordinates", coordinates);
+      //
+      // for (var component in componentForm) {
+      //   document.getElementById(component).value = '';
+      //   document.getElementById(component).disabled = false;
+      // }
+      //
+      // // Get each component of the address from the place details
+      // // and fill the corresponding field on the form.
+      // for (var i = 0; i < place.address_components.length; i++) {
+      //   var addressType = place.address_components[i].types[0];
+      //   if (componentForm[addressType]) {
+      //     var val = place.address_components[i][componentForm[addressType]];
+      //     document.getElementById(addressType).value = val;
+      //   }
+      // }
     }
 
     // Bias the autocomplete object to the user's geographical location,
     // as supplied by the browser's 'navigator.geolocation' object.
-    rpc.geolocate = function() {
+    function geolocate() {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
           console.log(position);
