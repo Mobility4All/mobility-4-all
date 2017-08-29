5023e9724a07e960444582ec58667e1ac6c2a827
jamesleary96@yahoo.com <jamesleary96@yahoo.com>
2017-08-29 15:10:00 -0500

    alert dialog working

--- server/public/scripts/controllers/ride-notification-rider.controller.js
+++ server/public/scripts/controllers/ride-notification-rider.controller.js
@@ -1,5 +1,35 @@
-myApp.controller('RiderNotificationController', function() {
+myApp.controller('RiderNotificationController', function($mdDialog, $scope) {
     console.log('RiderNotificationController created');
     var rc = this;
+    rc.driver = "james";
+    var eta = "10";
 
+    $scope.showDriverArrivalAdvanced = function(ev) {
+        $mdDialog.show({
+          controller: DialogController,
+          templateUrl: 'views/partials/arrive.dialog.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose:true,
+          fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+        })
+        .then(function(answer) {
+          $scope.status = answer;
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+      };
+      function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
 });
