b999561b5c6e21c3ec5b85c35d32449994bd2fae
Tom Brossart <tom@blueraven.digital>
2017-08-31 10:43:38 -0500

    matching query is almost all functional

--- server/routes/trip.router.js
+++ server/routes/trip.router.js
@@ -5,12 +5,17 @@ var pool = require('../modules/pool.js');
 
 router.get('/match', function(req, res, next) {
   console.log('matching ride', req.user);
-  var queryText = ['SELECT * FROM drivers WHERE live = true'];
-  if(req.user.elec_wheelchair) queryText.push('elec_wheelchair = true');
-  if(req.user.col_wheelchair) queryText.push('col_wheelchair = true');
-  if(req.user.service_animal) queryText.push('service_animal = true');
-  if(req.user.oxygen) queryText.push('oxygen = true');
-  queryText = queryText.join(' AND ')
+  // 'how to get riders lng/lat from this route...'
+  var rider = {};
+  rider.lng = -74.23;
+  rider.lat = 45;
+  var queryText = ['SELECT *, id FROM drivers  WHERE live = true'];
+  if(req.user.elec_wheelchair) queryText.push(' AND elec_wheelchair = true');
+  if(req.user.col_wheelchair) queryText.push(' AND col_wheelchair = true');
+  if(req.user.service_animal) queryText.push(' AND service_animal = true');
+  if(req.user.oxygen) queryText.push(' AND oxygen = true');
+  queryText.push('ORDER BY location <-> st_setsrid(st_makepoint($1,$2),4326) LIMIT 10');
+  queryText = queryText.join(' ');
   console.log('query text', queryText);
   if(req.isAuthenticated()) {
     pool.connect(function(err, client, done) {
@@ -18,7 +23,7 @@ router.get('/match', function(req, res, next) {
         console.log("Error connecting: ", err);
         next(err);
       }
-      client.query(queryText, function (err, result) {
+      client.query(queryText, [rider.lng, rider.lat], function (err, result) {
         done();
 
         if(err) {
