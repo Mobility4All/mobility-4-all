34341de695377bf88df0a05203bfa6b136012eed
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-08-31 12:41:07 -0500

    updated map size, added reverse geolocation function

--- server/public/scripts/controllers/default-view.controller.js
+++ server/public/scripts/controllers/default-view.controller.js
@@ -19,34 +19,37 @@ myApp.controller('DefaultViewController', function($http, $timeout, $mdBottomShe
 
   // These functions take in user input for start and end destinations, and returns
   //a google map with polyline route and with text driving directions
-
   dc.startAndEndInput = {
     start: '',
     end: ''
   }
 
-  //set directions panel div to be an angular element, clear and reset after each new directions req
+  //clear and reset the text directions after each new directions request
   dc.panelEl = angular.element( document.querySelector( '#right-panel' ) );
 
-
+//on click, old req is cleared, new request is called with initMap() to get and show new directions
   dc.onClick = function() {
     dc.panelEl.empty();
     initMap();
+    geocodeLatLng(geocoder, infowindow);  //REMOVED map param
     console.log(dc.start);
     console.log(dc.end);
   };
 
+//this google maps function gets directions and displays on a map and with text
   function initMap() {
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
-      center: {lat: 41.85, lng: -87.65}
+      zoom: 7,  // this resets after directions are received. Might update if we want to change initial view.
+      center: {lat: 41.85, lng: -87.65} //Might update if we want to change initial view.
     });
     directionsDisplay.setMap(map);
+    //setPanel displays the written driving directions
     directionsDisplay.setPanel(document.getElementById('right-panel'));
     calculateAndDisplayRoute(directionsService, directionsDisplay);
-    //get rid of this code and use data binding with angular instead for start and end point
+    //calculateAndDisplayRoute shows map and line drawing of route
+
 
     function calculateAndDisplayRoute(directionsService, directionsDisplay) {
       directionsService.route({
@@ -58,22 +61,23 @@ myApp.controller('DefaultViewController', function($http, $timeout, $mdBottomShe
           directionsDisplay.setDirections(response);
           console.log('status ok response', response);
         } else {
-          window.alert('Directions request failed due to ' + status);
+          console.log('Directions request failed due to ' + status);
         }
       });
     }
+  } //end of google map direction function
 
-  } //end of init map function
 
-  //HTML 5 geolocation pure JS
-
+  //HTML 5 geolocation code begins here
   dc.message = '';
   dc.coords = {
     lat: '',
     lng: ''
   };
 
-
+// geoLocate called on click. getLocation checks for browser compatability (user must approve to enable),
+//then getLocation calls showPosition(), which gets coords. Then showPosition() calls updateDriverLocation()
+//to make a put request to database with the driver location.
   dc.geoLocate = function() {
     console.log('update location function called');
 
@@ -129,5 +133,35 @@ dc.showGridBottomSheet = function() {
       console.log('update location -- success', response);
     })
   } //end put req
+
+
+//REVERSE GEOCODE CODE
+  // create new GeoCoder
+    var geocoder = new google.maps.Geocoder;
+    var infowindow = new google.maps.InfoWindow;
+
+//i removed the maps param from the function below
+    function geocodeLatLng(geocoder, infowindow) {
+
+
+        var input = '44.9780310,-93.2635010';
+        // can test with other input, it works: 40.714224,-73.961452
+
+        var latlngStr = input.split(',', 2);
+        var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+        geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[0]) {
+              infowindow.setContent(results[0].formatted_address);
+              // infowindow.open(map, marker);
+              console.log('hopefully this is primes address', results[0].formatted_address);
+            } else {
+              console.log('No results found');
+            }
+          } else {
+            console.log('Geocoder failed due to: ' + status);
+          }
+        });
+      }
 
 }); //end of controller
