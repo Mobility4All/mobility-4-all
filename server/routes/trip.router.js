bd7d218d6f29bae73bdf745aac9ff0274b32b4e4
Tom Brossart <tom@blueraven.digital>
2017-08-31 13:51:32 -0500

    confirmed query is fully operational

--- server/routes/trip.router.js
+++ server/routes/trip.router.js
@@ -3,23 +3,16 @@ var router = express.Router();
 var path = require('path');
 var pool = require('../modules/pool.js');
 
+// match rider to driver based on (1) driver being live, (2) specific needs, (3) 5 drivers closest to rider
 router.get('/match', function(req, res, next) {
   console.log('matching ride', req.user);
-  // 'how to get riders lng/lat from this route...'
-  // SELECT start_location FROM trips WHERE rider_id = $1 AND complete = FALSE;
-  // [req.user.id]
-  // JOIN this
-  // var rider = {};
-  // rider.lng = -74.23;
-  // rider.lat = 45;
-  // WITH rider_lng AS (ST_X(start_location::geometry) FROM trips),rider_lat AS (ST_Y(start_location::geometry) FROM trips;)
-  var queryText = ['WITH rider_lng AS (SELECT ST_X(start_location::geometry) AS rlng FROM trips WHERE rider_id = $1), rider_lat AS (SELECT ST_Y(start_location::geometry) AS rlat FROM trips WHERE rider_id = $1) SELECT *, id FROM drivers WHERE live = true'];
+  var queryText = ['WITH rider_lng AS (SELECT ST_X(start_location::geometry) AS rlng FROM trips WHERE complete = FALSE AND rider_id = $1), rider_lat AS (SELECT ST_Y(start_location::geometry) AS rlat FROM trips WHERE complete = FALSE AND rider_id = $1) SELECT *, id FROM drivers WHERE live = true'];
   if(req.user.elec_wheelchair) queryText.push(' AND elec_wheelchair = true');
   if(req.user.col_wheelchair) queryText.push(' AND col_wheelchair = true');
   if(req.user.service_animal) queryText.push(' AND service_animal = true');
   if(req.user.oxygen) queryText.push(' AND oxygen = true');
   // HOW TO PASS rider_lng and rider_lat into st_makepoint???
-  queryText.push('ORDER BY location <-> st_setsrid(st_makepoint((SELECT rlng FROM rider_lng), (SELECT rlat FROM rider_lat)),4326) LIMIT 10');
+  queryText.push('ORDER BY location <-> st_setsrid(st_makepoint((SELECT rlng FROM rider_lng), (SELECT rlat FROM rider_lat)),4326) LIMIT 5');
   queryText = queryText.join(' ');
   console.log('query text', queryText);
   // console.log('rider_lng', rider_lng);
@@ -37,7 +30,7 @@ router.get('/match', function(req, res, next) {
           console.log("Error inserting data: ", err);
           res.sendStatus(500);
         } else {
-          res.send({drivers: result.rows})
+          res.send({drivers: result.rows});
         }
       });
     });
