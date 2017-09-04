24a08140e0765bf99ab8ffb627b372a2cd4b8bb8
Tom Brossart <tom@blueraven.digital>
2017-09-04 14:21:44 -0500

    can start and end interval from DOM

--- server/routes/trip.router.js
+++ server/routes/trip.router.js
@@ -3,10 +3,12 @@ var router = express.Router();
 var passport = require('passport'); // probably unnecessary but moving this over from eta.router.js
 var path = require('path');
 var pool = require('../modules/pool.js');
+var matchCountdown;
+
 
  router.matched = function() {
    console.log("driver and rider matched, terminate the loop");
-   clearInterval(timeout);
+   clearInterval(matchCountdown);
  };
 
  function offerDriverRide() {
@@ -16,7 +18,7 @@ var pool = require('../modules/pool.js');
 // match rider to driver based on (1) driver being live, (2) specific needs, (3) 5 drivers closest to rider
 router.get('/match', function(req, res, next) {
   console.log('matching ride', req.user);
-  console.log('req.socket', req.socket);
+  // console.log('req.socket', req.socket);
   var queryText = ['WITH rider_lng AS (SELECT ST_X(start_location::geometry) AS rlng FROM trips WHERE complete = FALSE AND rider_id = $1), rider_lat AS (SELECT ST_Y(start_location::geometry) AS rlat FROM trips WHERE complete = FALSE AND rider_id = $1) SELECT *, id FROM drivers WHERE live = true'];
   if(req.user.elec_wheelchair) queryText.push(' AND elec_wheelchair = true');
   if(req.user.col_wheelchair) queryText.push(' AND col_wheelchair = true');
@@ -42,8 +44,7 @@ router.get('/match', function(req, res, next) {
         } else {
           req.io.to(result.rows[0].driver_socket).emit('find-driver', req.user);
           res.send({drivers: result.rows});
-          setInterval(offerDriverRide(),1000);
-
+          matchCountdown = setInterval(offerDriverRide,1000);
 
 
 
