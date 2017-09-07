7d857f62e8ae6e64e7c5d9e90cadbbcfb9bc6246
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-07 11:59:06 -0500

    cleaned up controllers

--- server/public/scripts/controllers/address-controller.js
+++ server/public/scripts/controllers/address-controller.js
@@ -1,4 +1,4 @@
-myApp.controller('AddressController', function($http, $location, UserService, DataService, NavigationService, NgMap) {
+myApp.controller('AddressController', function(NavigationService) {
     console.log('RiderLoginController created');
     var ac = this;
     ac.navigationService = NavigationService;
