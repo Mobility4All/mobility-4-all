0a77e0d7b88bed0c0c31d4cb514b05f6577c7a67
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-13 17:02:35 -0500

    code comments

--- server/modules/key.config.js
+++
@@ -1,8 +0,0 @@
-APIKeys = {
-  distanceMatrixKey: 'AIzaSyCLk7z7EvTPDatApnjpiNWXkNCGct_K1q8',
-  directionsWebServiceKey: 'AIzaSyAUYFAt5CiiFnXi_bz8p6JDMD01ssoQ73M',
-  placesWebServiceKey: 'AIzaSyA_yzOfP3ey4J3-QvI36s9aYwBf4FCFkLs'
-}
-
-
-module.exports = APIKeys;
